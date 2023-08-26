15e28b501bafe38a3dc401f034c040c982bfdfe9
Filippo Arceci <100956831+positroniQuo@users.noreply.github.com>
2023-08-26 11:15:42 +0200

    Update code_4.r

--- code_4.r
+++ code_4.r
@@ -1,17 +1,23 @@
 # setting a working directory in my pc for this code
+
 setwd("D:\Uni\Progetti_R")
 
 # package for environmental analysis'
+
 library(vegan)
 
 # to import a complete r project the "load()" function is required. The project needs to be located in the wd
 # the following data contains two tables: biomes.cv and biometypes.cv
+
 load("biomes_multivar.RData") 
 
 # "decorana()" (detrended correspondence analysis) is the function for multivariate analysis that we will use
 # this simplifies the data by creating new data dimensions in a matrix, just like in a PCA
+
 multivar <- decorana(biomes) 
+
 # to show the properties of the matrix
+
 multivar 
 
 # Call:
@@ -30,16 +36,21 @@ plot(multivar) # use function ~plot() to visualize the matrix created by decoran
 
 # "attach()" calls for the biome types contained in the second table and "ordiellipse()" adds a circle around points of the same biome,
 # and specify the table they belong to. "kind = ehull" makes a specific shape of ellipse, lwd is the line width
+
 attach(biomes_types)
+
 ordiellipse(multivar, type, col = c("brown4", "green4", "azureblue4", "yellow4"), kind = "ehull", lwd = 2)
 
-# now use function ~ordispider()  to attach the labels to the circles in a net form (like a spider :D)
-attach(biomes_types)
-ordispider(multivar, type, col = c("dodgerblue4", "deeppink3", "darkseagreen4", "mediumpurple2"), label = T)
+# ordispider attaches the lables to the circles
 
-# to save the plot in a pdf format use function ~pdf("")
+attach(biomes_types)
+
+ordispider(multivar, type, col = c("brown4", "green4", "azureblue4", "yellow4"), label = T)
+
+# saving the plot as a pdf
+
 pdf("multivar.pdf")
 plot(multivar)
-ordiellipse(multivar, type, col = c("dodgerblue4", "deeppink3", "darkseagreen4", "mediumpurple2"), kind = "ehull", lwd = 2)
-ordispider(multivar, type, col = c("dodgerblue4", "deeppink3", "darkseagreen4", "mediumpurple2"), label = T)
+ordiellipse(multivar, type, col = c("brown4", "green4", "azureblue4", "yellow4"), kind = "ehull", lwd = 2)
+ordispider(multivar, type, col = c("brown4", "green4", "azureblue4", "yellow4"), label = T)
 dev.off()
