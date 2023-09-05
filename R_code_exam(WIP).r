fe977d8e0ca3240d98fe523f60da2a85413d8433
Filippo Arceci <100956831+positroniQuo@users.noreply.github.com>
2023-09-05 14:50:54 +0200

    Update R_code_exam(WIP).r

--- R_code_exam(WIP).r
+++ R_code_exam(WIP).r
@@ -31,9 +31,11 @@ library(sp) # package for importing, manipulating and exporting spatial data
 library(viridis) # beautiful and colorblind inclusive colour patterns
 library(RStoolbox) # to bypass the "fortify()" functionc coherction limitations
 library(sf) # used to make polygons a simple feature for manipulations
+library(scales) # to make a graph show relative instead of absolute values
+
 # reading the raster of the soil water index (SWI)
 
-swi <- brick("c_gls_SWI1km_202005011200_CEURO_SCATSAR_V1.0.1.nc", varname = "SWI_002") # brick creates a raster object
+swi <- brick("c_gls_SWI1km_202007021200_CEURO_SCATSAR_V1.0.1.nc", varname = "SWI_002") # brick creates a raster object
 
 # let's see if it has been imported correctly
 
@@ -49,40 +51,86 @@ er
 
 extent(er)
 
+crs(er) <-"+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+
 swi_c <- crop(swi, extent(er))
 
 plot(swi_c)
 
-swi_er <- mask(swi_c, er)
+swi_m <- mask(swi_c, er)
 
 # time to plot the result using ggplot and a viridis colour scale
+# water content ranges from 1 to 100%, but the digital values in this dataset range from 12 to 126
+# dividing by the max and multiplying time 100 returns the % scale
+
+swi_100 <- (swi_m/126)*100
 
 ggplot() +
-  geom_raster(swi_er, mapping=aes(x=x,y=y,fill=X2020.05.01)) +
-  scale_fill_viridis(option = "G") +
-  labs(fill = 'Water Content') +
-  ggtitle("SWI Emilia Romagna") 
+  geom_raster(swi_100, mapping=aes(x=x,y=y,fill=X2020.07.02)) +
+  scale_fill_viridis(option = "mako") +
+  labs(fill = 'Water Content %') +
+  ggtitle("SWI Emilia Romagna") +
+  theme(legend.position = c(.26, .32),
+        legend.justification = c("right", "top"),
+        legend.box.just = "right",
+        legend.margin = margin(5, 5, 5, 5))
  
 # I will do the same passeges on the forest cover file now. Please nothe that the "raster" 
-# function can be used as well as "brick" to import the data.
+# function can be used as well as "brick" to import the data. Plotting will be skipped as
+# this is a particularly heavy dataset
 
-fc <- raster("c_gls_FCOVER-RT2_202005310000_GLOBE_PROBAV_V2.0.1.nc")
-
-plot(fc)
+fc <- raster("c_gls_FCOVER300-RT6_202007100000_GLOBE_OLCI_V1.1.1.nc")
 
 fc
 
 fc_c <- crop(fc, extent(er))
 
-fc_er <- mask(fc_c, er)
+fc_m <- mask(fc_c, er)
 
-fc_er
+fc_m
 
 ggplot() +
-  geom_raster(fc_er, mapping=aes(x=x,y=y,fill=Fraction.of.green.Vegetation.Cover.1km)) +
+  geom_raster(fc_m, mapping=aes(x=x,y=y,fill=Fraction.of.green.Vegetation.Cover.333m)) +
+  scale_fill_viridis(option = "A") +
+  labs(fill = 'Forest Cover %area') +
+  ggtitle("FC Emilia Romagna")  +
+  theme(legend.position = c(.35, .32),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.margin = margin(5, 5, 5, 5))
+
+
+# for last, the surface temperature. I will only consider the median temperature for the 10 day data gather period
+
+stemp <- brick("c_gls_LST10-DC_202007010000_GLOBE_GEO_V1.2.1.nc", varname = "MEDIAN")
+
+stemp_c <- crop(stemp, extent(er))
+
+stemp_m <- mask(stemp_c, er)
+
+stemp_m 
+
+# I switch temperature scale from °k to °C
+
+stemp_C <- stemp_m - 272.15
+
+stemp_C
+
+# the LST-V1 files contain the mean temperature from 10 days of observations, for each hour of the day.
+# since we need an indicative temperature amount for the area a mean of the temperatures during the days
+# will be calculated and used as graphical showcase
+
+mean_t <- calc(stemp_C, fun = mean, na.rm = T)
+
+ggplot() +
+  geom_raster(mean_t, mapping=aes(x=x,y=y,fill=layer)) +
   scale_fill_viridis(option = "H") +
-  labs(fill = 'Forest Cover') +
-  ggtitle("FC Emilia Romagna")
+  labs(fill = 'Temperature in °C') +
+  ggtitle("surface temperature") +
+  theme(legend.position = c(.34, .32),
+        legend.justification = c("right", "top"),
+        legend.box.just = "right",
+        legend.margin = margin(5, 5, 5, 5))
 
 # I am now importing the wildfires dataset as a raster
 
@@ -103,29 +151,44 @@ extent(fires)
 # making the plots semi-transparents we can see how theu all stack up
 # the fires shapefile is treated separately as will be explained in the following lines
 
+# the final image will be exported as a .pdf
+
+
+fc_er <- resample(fc_m, raster(extent(er), ncol=500, nrow=500))
+swi_er <- resample(swi_100, raster(extent(er), ncol=500, nrow=500))
+mean_t_er <- resample(mean_t, raster(extent(er), ncol=500, nrow=500))
+
+env <- list(fc_er, swi_er, mean_t_er)
+
 noir <- colorRampPalette(c("white","black"))(100)
 
-plot(fc_er, col = alpha(noir, 0.3))
+box=extent(er)
 
-plot(swi_er, col = alpha(noir, 0.3), add = T)
+yxscale=(box@ymax-box@ymin)/(box@xmax-box@xmin-1)
 
-# adding the region boundaries for aestetics
+xsize=5
 
-plot(er, add = T)
+ysize=xsize*yxscale
 
-# fires are represented in a shp containing the area that burnt down for each.
-# these areas are very small compared to the regional extention, so to better
-# visualize them I calculated the centroids of every polygon representing a fire
-# and plotted it so that its dimentions would be dependant on its area.
-# Therefore, bigger circles on the plot represent bigger fires.
-# The centroids are calculated taking each element of the 103 contained in the
-# "fires" SpatialPolygonsDataFrame, making it a simple feature, getting the centroid
-# coordinates and making it a spatial element again for plotting.
+par(pin=c(xsize,ysize))
 
-for (i in 1:length(fires)) {
-  plot( fires[i,] %>% 
-    sf::st_as_sf() %>%          
-    sf::st_centroid() %>% 
-    as(.,'Spatial'),
-    cex = fires[i,]$Shape_Area/20000, pch = 21, col = "red", bg = alpha("yellow", alpha = 0.3), add = T)
-}
+jpeg("Fires_X_env.jpeg")
+
+par(pin=c(xsize,ysize))
+
+  plot(er, main = "Fires and environmental variables", cex.main = 1.5)
+
+  image(env[[1]], col = alpha(noir, 0.4), add= T)
+  
+  image(env[[2]], col = rev(alpha(noir, 0.4)), add= T)
+  
+  image(env[[3]], col = alpha(viridis(100, option = "turbo"), 0.2), add= T)
+  
+  for (i in 1:length(fires)) {
+    plot( fires[i,] %>% 
+            sf::st_as_sf() %>%          
+            sf::st_centroid() %>% 
+            as(.,'Spatial'),
+          cex = fires[i,]$Shape_Area/15000, pch = 21, col = "red", bg = alpha("yellow", alpha = 0.3), add = T) }
+
+dev.off()
